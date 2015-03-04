798e2d693b2c4442c246e7777ca93cac09217a0a
kirk7880@gmail.com <kirk7880@gmail.com>
2015-03-03 19:12:48 -0500

    Added delete function

--- src/cache.js
+++ src/cache.js
@@ -4,205 +4,199 @@ const _private = new require(__dirname + '/private')();
 const Item = require(__dirname + '/item');
 
 /**
- * Default cache size to use which
- * is 20 Megabytes.
- *
- * @type {Number} Bytes
- */
+* Default cache size to use which
+* is 20 Megabytes.
+*
+* @type {Number} Bytes
+*/
 const DEFAULT_CACHE_SIZE = 20000000;
 
 var map = new Map();
 
 /**
- * LRUCache
- * @constructor
- * @param {Number} size Cache size in bytes
- */
+* LRUCache
+* @constructor
+* @param {Number} size Cache size in bytes
+*/
 var LRUCache = function(size) {
-    if (!(this instanceof LRUCache)) {
-        return new LRUCache(size);
-    }
+  if (!(this instanceof LRUCache)) {
+      return new LRUCache(size);
+  }
 
-    _private(this)._maxCacheSize = size || DEFAULT_CACHE_SIZE;
-    _private(this)._length = 0;
-    _private(this)._cacheSize = 0;
+  _private(this)._maxCacheSize = size || DEFAULT_CACHE_SIZE;
+  _private(this)._length = 0;
+  _private(this)._cacheSize = 0;
 };
 
 /**
- * Creates a new Cache item.
- *
- * @param item
- * @param ttl
- * @returns {Item}
- */
+* Creates a new Cache item.
+*
+* @param item
+* @param ttl
+* @returns {Item}
+*/
 LRUCache.prototype.createItem = function(item, ttl) {
-    return new Item(item, ttl);
+  return new Item(item, ttl);
 };
 
 LRUCache.prototype.replaceLRU = function(key, item, ttl) {
-    var keys = Object.keys(map);
-    var len = keys.length;
-    var _key = null;
-    var count = 0;
+  var keys = Object.keys(map);
+  var len = keys.length;
+  var _key = null;
+  var count = 0;
 
-    for (var i=0; i<len; i++) {
-        var c = map[keys[i]].count;
-        if (!count || c < count) {
-            count = c
-            _key = keys[i];
-        }
-    }
+  for (var i=0; i<len; i++) {
+      var c = map[keys[i]].count;
+      if (!count || c < count) {
+          count = c
+          _key = keys[i];
+      }
+  }
 
-    if (_key) {
-        map.delete(_key);
-    }
+  if (_key) {
+      map.delete(_key);
+  }
 
-    map.set(key, this.createItem(item, ttl));
+  map.set(key, this.createItem(item, ttl));
 }
 
 
 /**
- * Adds an item to the cache if it doesn't
- * already exists
- *
- * @type {Function}
- */
+* Adds an item to the cache if it doesn't
+* already exists
+*
+* @type {Function}
+*/
 LRUCache.prototype.put = LRUCache.prototype.set = function(key, item, ttl) {
-    if (map.has(key)) {
-        return;
-    }
+  if (map.has(key)) {
+      return;
+  }
 
-    if (this.isFull()) {
-        return this.replaceLRU(key, item, ttl);
-    }
+  if (this.isFull()) {
+      return this.replaceLRU(key, item, ttl);
+  }
 
-    var data = this.createItem(item, ttl);
+  var data = this.createItem(item, ttl);
 
-    map.set(key, data);
-    this.updateCacheSize(data);
-    this._length += 1;
-    return true;
+  map.set(key, data);
+  this.updateCacheSize(data);
+  this._length += 1;
+  return true;
 };
 
 /**
- * Returns an item from the cache if it exists.
- *
- * @param key
- * @returns {*}
- */
+* Returns an item from the cache if it exists.
+*
+* @param key
+* @returns {*}
+*/
 LRUCache.prototype.get = function(key) {
-    if (!this.exists(key)) {
-        return null;
-    }
+  if (!this.exists(key)) {
+      return null;
+  }
 
-    var item = map.get(key);
-    item.accessCount += 1;
+  var item = map.get(key);
+  item.accessCount += 1;
 
-    return item;
+  return item;
 };
 
 /**
- * Get the size of the item in bytes
- *
- * @param {Mized} item
- * @returns {Number}
+ * Deletes a key from the cache if it 
+ * exists.
  */
+LRUCache.prototype.delete = function(key) {
+  return map.delete(key);
+};
+
+/**
+* Get the size of the item in bytes
+*
+* @param {Mized} item
+* @returns {Number}
+*/
 LRUCache.prototype.getItemSize = function(item) {
-    var str = item;
+  var str = item;
 
-    if (typeof (item) === 'object') {
-        str = JSON.stringify(item);
-    } else if (typeof (item) === 'number') {
-        str = '' + item;
-    } else if (typeof (item) === 'function') {
-        str = item.toString();
-    }
+  if (typeof (item) === 'object') {
+      str = JSON.stringify(item);
+  } else if (typeof (item) === 'number') {
+      str = '' + item;
+  } else if (typeof (item) === 'function') {
+      str = item.toString();
+  }
 
-    return Buffer.byteLength(str);
+  return Buffer.byteLength(str);
 };
 
 /**
- * Returns the current size of the cache.
- *
- * @returns {number} Size of the cache in bytes
- */
+* Returns the current size of the cache.
+*
+* @returns {number} Size of the cache in bytes
+*/
 LRUCache.prototype.getCacheSize = function() {
-    return this._cacheSize;
+  return this._cacheSize;
 };
 
 /**
- * Gets the max cache size in bytes
- * @returns {Number|*}
- */
+* Gets the max cache size in bytes
+* @returns {Number|*}
+*/
 LRUCache.prototype.getMaxCacheSize = function() {
-    return this._maxCacheSize;
+  return this._maxCacheSize;
 };
 
 /**
- * Updates the cache size.
- *
- * @param size
- */
+* Updates the cache size.
+*
+* @param size
+*/
 LRUCache.prototype.updateCacheSize = function(data) {
-    var size = this.getItemSize(data);
-    this._cacheSize += size;
+  var size = this.getItemSize(data);
+  this._cacheSize += size;
 };
 
 /**
- * Checks if the cache is at max capacity.
- *
- * @returns {boolean}
- */
+* Checks if the cache is at max capacity.
+*
+* @returns {boolean}
+*/
 LRUCache.prototype.isFull = function() {
-    return this.getCacheSize() >= this.getMaxCacheSize();
+  return this.getCacheSize() >= this.getMaxCacheSize();
 }
 
 /**
- * Returns the current timestamp
- * @returns {number}
- */
+* Returns the current timestamp
+* @returns {number}
+*/
 LRUCache.prototype.now = function() {
-    return new Date().getTime();
+  return new Date().getTime();
 };
 
 /**
- * Checks if an item is expired in
- * the cache.
- *
- * @param {Mixed} item
- * @returns {boolean}
- */
+* Checks if an item is expired in
+* the cache.
+*
+* @param {Mixed} item
+* @returns {boolean}
+*/
 LRUCache.prototype.expired = function(item) {
-    return (item && (item.ttl < this.now()));
+  return (item && (item.ttl < this.now()));
 };
 
 /**
- * Checks if an item exists in the cache
- *
- * @param {String} key
- * @returns {*|boolean}
- */
+* Checks if an item exists in the cache
+*
+* @param {String} key
+* @returns {*|boolean}
+*/
 LRUCache.prototype.exists = function(key) {
-    var item = map.get(key);
-    return (item && !this.expired(item));
+  var item = map.get(key);
+  return (item && !this.expired(item));
 };
 
 LRUCache.prototype.size = function() {
-    return this._length;
+  return this._length;
 };
 
 module.exports = LRUCache;
-
-//var util = require('util');
-//var cache = new LRUCache();
-//var data = {name: 'kirk'};
-//var max = 1000000;
-//
-//var start = process.hrtime();
-//for (var i=0; i<max; i++) {
-//    cache.put(('' + i), data, 600);
-//}
-//
-//var end = process.hrtime(start);
-//
-//console.log('ops', Math.floor(max/end[0]))
